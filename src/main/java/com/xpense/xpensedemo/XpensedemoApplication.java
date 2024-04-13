cc6fd9357a8815c213435d24426db0633bb06c0e
Guilherme <guilherme.weidmann@gmail.com>
2024-04-13 20:41:20 -0300

    First commit

---
+++ src/main/java/com/xpense/xpensedemo/XpensedemoApplication.java
@@ -0,0 +1,13 @@
+package com.xpense.xpensedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpensedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpensedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpense/xpensedemo/XpensedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpense.xpensedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpensedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
